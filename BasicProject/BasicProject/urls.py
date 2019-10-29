16111f5a404a5965166353d28d93bb35409aaa9f
Fernando Garza <nando.garza@hotmail.com>
2019-10-29 08:44:55 -0400

    Work with "extends"

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
-
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 
 ]
 
