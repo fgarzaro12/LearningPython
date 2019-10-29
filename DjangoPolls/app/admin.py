29f2081f5733f4e405fca036a77dce487e58d29f
Fernando Garza <nando.garza@hotmail.com>
2019-10-29 19:03:06 -0400

    Playing wiht Polls code

--- DjangoPolls/app/admin.py
+++ DjangoPolls/app/admin.py
@@ -14,6 +14,7 @@ class PollAdmin(admin.ModelAdmin):
     """Definition of the Poll editor."""
     fieldsets = [
         (None, {'fields': ['text']}),
+        (None, {'fields': ['author']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
